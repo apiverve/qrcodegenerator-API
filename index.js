033fd2e770f6873f1b8d32e790d8ed014b3e8a18
APIVerve Team <hello@apiverve.com>
2024-07-17 12:20:05 -0500

    Bug Fixes

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class qrcodegeneratorWrapper {
             query = {};
         }
 
-        var requiredParams = ["text"];
+        var requiredParams = ["value"];
         if (requiredParams.length > 0) {
             for (var i = 0; i < requiredParams.length; i++) {
                 if (!query[requiredParams[i]]) {
